041d63b20dbea2f28ab2762d2db1eea08e5d045f
Gabrielprogramist <gabitsekenov09@gmail.com>
2024-05-30 19:27:47 +0500

    Create build_trainset copy.sh

---
+++ build_dataset/build_trainset copy.sh
@@ -0,0 +1,7 @@
+python build_dataset/build_meta4train.py `
+--saving_dir results/your_task_name/ `
+--content_font content/content/arial `
+--train_font_dir content/train `
+--val_font_dir content/val `
+--seen_unis_file meta/train_unis.json `
+--unseen_unis_file meta/val_unis.json
